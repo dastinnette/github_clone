ba3c2b09623d38411243514b0f3552f8b3b4e98e
David Stinnette <dastinnette@gmail.com>
2016-02-16 14:17:20 -0700

    Create user model & initial migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#show'
 
   # get '/user', to 'user#show'
-  #
-  # get '/auth/:provider/callback', to: 'sessions#create'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
