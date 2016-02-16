853c263581b82ba8420ca1326d6458e0c2b6c8e8
David Stinnette <dastinnette@gmail.com>
2016-02-16 14:01:42 -0700

    Build welcome show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#show'
 
-  get '/user', to 'user#show'
-
-  get '/auth/:provider/callback', to: 'sessions#create'
+  # get '/user', to 'user#show'
+  #
+  # get '/auth/:provider/callback', to: 'sessions#create'
 end
