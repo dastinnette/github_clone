08ea376828a343f24e32e4a059ad4f899fe09834
David Stinnette <dastinnette@gmail.com>
2016-02-16 16:12:56 -0700

    Add user show page with dynamic user fields pulled from github

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#show'
 
-  # get '/user', to 'user#show'
+  get '/user', to: 'users#show'
 
   get '/auth/:provider/callback', to: 'sessions#create'
 end
