8a2d1a90a971ea30199499f30c21d3ac4d8a67e0
David Stinnette <dastinnette@gmail.com>
2016-02-16 14:25:49 -0700

    Add name to user model and find_or_create_from_auth method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216211418) do
+ActiveRecord::Schema.define(version: 20160216212409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160216211418) do
     t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
