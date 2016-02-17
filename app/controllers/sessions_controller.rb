c6377f800711bb8fcb043ad1fc3d061b20c97f07
David Stinnette <dastinnette@gmail.com>
2016-02-16 18:53:59 -0700

    Remove instance variables from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    if @user
-      session[:user_id] = @user.id
+    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    if user
+      session[:user_id] = user.id
       redirect_to user_path
     else
       redirect_to root_path
