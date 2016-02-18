aadf1dd3b6d5f9199ec4a41d04e017b69fda2047
David Stinnette <dastinnette@gmail.com>
2016-02-18 13:08:53 -0700

    User's total commits, longest & current streaks tests passing with VCR

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -34,4 +34,28 @@ RSpec.describe "GitHub API", type: :request do
     end
   end
 
+  it "returns a user's total commits" do
+    VCR.use_cassette("users_total_commits") do
+      service = GithubService.new(user).find_user_total_commits(user)
+
+      expect(service).to eq(856)
+    end
+  end
+
+  it "returns a user's current streak" do
+    VCR.use_cassette("users_current_streak") do
+      service = GithubService.new(user).find_user_current_streak(user)
+
+      expect(service).to eq(4)
+    end
+  end
+
+  it "returns a user's longest streak" do
+    VCR.use_cassette("users_longest_streak") do
+      service = GithubService.new(user).find_user_longest_streak(user)
+
+      expect(service).to eq(11)
+    end
+  end
+
 end
