fcf099b503eff8a157b61238ff8b7ee054b26492
David Stinnette <dastinnette@gmail.com>
2016-02-19 08:17:53 -0700

    Remove rspec/autorun for depreciation issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'vcr'
 require 'webmock'
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'simplecov'
 
 SimpleCov.start
